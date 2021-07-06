d76ab13a12b4aee84752e46a1c8832485e81ac7f
abbeyroyse13 <heatherruan126@gmail.com>
2021-07-06 15:37:21 -0500

    chapters 1&2 done

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css"
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">Jackson Rippner</h3>
+        <div className="customer__address">Address: 666 HellFire Lane</div>
+    </section>
+)
