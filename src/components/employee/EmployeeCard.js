30c32ab8b9231520dd737642efd11612365776cd
abbeyroyse13 <heatherruan126@gmail.com>
2021-07-16 20:59:37 -0500

    employee details

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -1,9 +1,14 @@
 import React from "react"
 import "./Employee.css"
+import { Link } from "react-router-dom"
 
 export const EmployeeCard = ({ employee }) => (
     <section className="employee">
-        <h3 className="employee__name">{employee.name}</h3>
-        <address className="location__address">{employee.location.name}</address>
+        <h3 className="employee__name">
+          <Link to={`/employees/detail/${employee.id}`}>
+            { employee.name }
+          </Link>
+        </h3>
+        <div className="employee__location">{ employee.address }</div>
     </section>
 )
